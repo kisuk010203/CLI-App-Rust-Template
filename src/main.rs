5fb60abf324bf37de92cebb14eaf56b6aa1f65b4
kisuk010203 <chriskisuk@gmail.com>
2024-03-15 01:02:11 +0900

    [Basic CLI] Use `anyhow` to return errors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::fs::File;
-use std::io::BufRead;
-use std::io::BufReader;
+use std::io::{BufRead, BufReader};
+use std::io::{self, Write};
 
+use anyhow::{Context, Result};
 use clap::Parser;
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -11,14 +12,22 @@ struct Cli {
     /// The path to the file to read
     path: std::path::PathBuf,
 }
-fn main() {
+fn main() -> Result<()> {
+    let stdout = io::stdout();
+    let mut handle = io::BufWriter::new(stdout.lock());
+
     let args = Cli::parse();
-    let content = File::open(&args.path).expect("Could not read file");
+
+    let content =
+        File::open(&args.path).with_context(|| format!("Could not read file {:?}", &args.path))?;
     let lines = BufReader::new(content).lines();
+
     for line in lines {
         let real_line = line.unwrap();
         if real_line.contains(&args.pattern) {
-            println!("{}", real_line);
+            writeln!(handle, "{}", real_line)?;
         }
     }
+
+    Ok(())
 }
