b6dc1b60b51f9a4282a8238f90bae4c6f665bb1a
kisuk010203 <chriskisuk@gmail.com>
2024-03-15 00:44:11 +0900

    [Basic CLI] Get `pattern`, `path` and output lines with `pattern` in `path`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+use clap::Parser;
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    let content = File::open(&args.path).expect("Could not read file");
+    let lines = BufReader::new(content).lines();
+    for line in lines {
+        let real_line = line.unwrap();
+        if real_line.contains(&args.pattern) {
+            println!("{}", real_line);
+        }
+    }
 }
